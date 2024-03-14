e49661c40706e85b3226ef0598bf8ccf742a7c43
huuvuno1 <huuvu@telio.vn>
2024-03-14 13:19:33 +0700

    feat: add banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,12 +1,45 @@
-import Image from 'next/image';
-import saleBanner from '@/assets/images/sale-banner.jpg';
+"use client";
+import NextIcon from "@/assets/icons/next.png";
+import PrevIcon from "@/assets/icons/previous.png";
+import Banner1 from "@/assets/images/banner1.png";
+import Image from "next/image";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import "./style.css";
+
+const buttonStyle = {
+  width: "30px",
+  background: "none",
+  border: "0px",
+};
+
+const properties = {
+  prevArrow: (
+    <button style={{ ...buttonStyle }}>
+      <Image width={59} height={59} src={PrevIcon} alt="" />{" "}
+    </button>
+  ),
+  nextArrow: (
+    <button style={{ ...buttonStyle }}>
+      <Image width={59} height={59} src={NextIcon} alt="" />
+    </button>
+  ),
+};
 
 export const Banner = () => {
   return (
-    <div className="banner">
-      <div className="banner__content">
-        <Image src={saleBanner} width={850} height={400} alt="Shopping cart" />
-      </div>
+    <div className="w-full h-fit">
+      <Slide {...properties} indicators={true}>
+        <div className="each-slide-effect">
+          <Image src={Banner1} alt="" />
+        </div>
+        <div className="each-slide-effect">
+          <Image src={Banner1} alt="" />
+        </div>
+        <div className="each-slide-effect">
+          <Image src={Banner1} alt="" />
+        </div>
+      </Slide>
     </div>
   );
 };
